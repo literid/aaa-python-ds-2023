d809ad6c924dc2c7be753f285734a7845f633956
literid <hohlovkola@gmail.com>
2022-09-24 22:01:15 +0300

    handling of price property

--- homework2/Advert.py
+++ homework2/Advert.py
@@ -8,10 +8,11 @@ class Advert:
                 raise TypeError("keys have to be strings")
             if key == 'title':
                 title_flag = True
+            if key == 'price' and mapping[key] < 0:
+                raise ValueError("price have to be >= 0")
 
             # if mapping[key] is dict we will create another class,
             # that stores values of this
-
             if isinstance(mapping[key], dict):
                 # create a class, which stores inner dict
                 class_storer = type(key, (), mapping[key])
@@ -22,6 +23,15 @@ class Advert:
         if not title_flag:
             raise RuntimeError("no 'title' key in mapping")
 
+    @property
+    def price(self):
+        if 'price' not in self.__dict__:
+            return 0
+
+    def __repr__(self):
+        return f"{self.title} | {self.price} ₽"
+
 
 ad = Advert({"title": 'example', 'price': 100, 'location': {'address': 'Alise in the mirror 12', 'house': 26}})
 print(ad.location.house)
+print(ad)
