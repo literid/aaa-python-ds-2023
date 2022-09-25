18cbb9a563e1035e8662f96a9f805db55fd87e7d
literid <hohlovkola@gmail.com>
2022-09-25 13:15:24 +0300

    add colorizer mixin

--- homework2/Advert.py
+++ homework2/Advert.py
@@ -4,7 +4,14 @@ except ImportError:
     from collections import Mapping
 
 
-class Advert:
+class ColorizerMixin:
+    repr_color_code = 33
+
+    def __repr__(self):
+        return f"\033[1;{self.repr_color_code};40m{self.title} | {self.price} ₽"
+
+
+class Advert(ColorizerMixin):
     def __init__(self, mapping):
         # check if 'title' in keys
         title_flag = False
@@ -20,11 +27,10 @@ class Advert:
                     raise ValueError("price have to be int or float")
                 if price_value < 0:
                     raise ValueError("price have to be >= 0")
-
                 price_key = '_price'
                 self.__dict__[price_key] = price_value
-
                 continue
+
             # if mapping[key] is dict we will create another class,
             # that stores values of this
             if isinstance(mapping[key], Mapping):
@@ -43,9 +49,6 @@ class Advert:
             return 0
         else:
             return self._price
-
-    def __repr__(self):
-        return f"{self.title} | {self.price} ₽"
 
 
 ad = Advert({"title": 'example', 'price': 100, 'location': {'address': 'Alise in the mirror 12', 'house': 26}})
